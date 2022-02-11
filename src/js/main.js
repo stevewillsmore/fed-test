8c982a7af5b1bc7db2769746dee373a425b24043
stevewillsmore <steve.willsmore@q4inc.com>
2022-02-11 11:45:12 -0500

    Initial commit
    FED Test ready to submit.

--- src/js/main.js
+++ src/js/main.js
@@ -5,12 +5,14 @@ var reportsWidget = {
             '{{#.}}' +
                 '<article class="reports_item">' +
                     '<a href="{{cover}}" target="_blank">' +
-                        '<img class="reports_cover" src="{{cover}}" alt="{{title}} Cover"/>'
-                    '</a>' +
+                        '<img class="reports_cover" src="{{cover}}" alt="{{title}} Cover"/>' +
+                    '<span class="sr-only">(this link opens in a new window)</span></a>' + // sr-only class added
                     '<footer class="reports_docs">' +
                         '{{#documents}}' +
                             '<h3 class="reports_title">' +
-                                '<a href="{{url}}" target="_blank">{{title}}</a>' +
+                                '<a href="{{url}}" target="_blank">{{title}}' +
+                                '<span>({{file_size}} {{file_type}})</span>' + // added file size/type missing from design
+                                '<span class="sr-only">(this link opens in a new window)</span></a>' + // sr-only class added
                             '</h3>' +
                         '{{/documents}}' +
                     '</footer>' +
@@ -19,8 +21,11 @@ var reportsWidget = {
         )
     },
 
+    // template was missing a '+', breaking the string concatenation and preventing the reports from populating the page
+
     init: function() {
         this.renderReports(reportData || []);
+        console.log(reportData); // just to see what was coming through to the page
     },
 
     renderReports: function(reports) {
